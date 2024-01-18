acaa612cde303131f6af7fe56b36845e3eb8ba1c
nanavjo <156484479+nanavjo@users.noreply.github.com>
2024-01-17 19:57:35 -0500

    Both files are modified in the repo clone
    - one  print() statement is deleted from script1.py
    -Two print() statements are added to script2.py

--- script1.py
+++ script1.py
@@ -1,3 +1,2 @@
 # 2 lines from Zen of Python
 print("Beautiful is better than ugly.")
-print("Explicit is better than implicit.")
--- script2.py
+++ script2.py
@@ -1,3 +1,4 @@
 # 2 scripting lines from Zen of Python
 print("Flat is better than nested.")
 print("Sparse is better than dense.")
+print("There should be one and preferably only one obvious way to do it.")
