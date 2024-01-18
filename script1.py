27758c134a7530422cc213944a6bb3198b933a23
nanavjo <156484479+nanavjo@users.noreply.github.com>
2024-01-17 19:10:19 -0500

    Revert "Scripting files with modification"
    This reverts commit e0a14da8f6881e6304adc81bac263f1228f0af85.

--- script1.py
+++ script1.py
@@ -1,2 +1,3 @@
 # 2 lines from Zen of Python
 print("Beautiful is better than ugly.")
+print("Explicit is better than implicit.")
--- script2.py
+++ script2.py
@@ -1,3 +1,2 @@
-# 2 scripting lines from Zen of Python 
 print("Flat is better than nested.")
 print("Sparse is better than dense.")
